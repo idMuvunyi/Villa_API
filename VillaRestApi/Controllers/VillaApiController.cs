9288db9cce76a1cac08d21ec7ecfab69b97c024b
idrissa <muvunyiiddy@gmail.com>
2023-11-08 15:38:51 +0200

    Update local data store endpoint

--- VillaRestApi/Controllers/VillaApiController.cs
+++ VillaRestApi/Controllers/VillaApiController.cs
@@ -10,18 +10,22 @@ namespace VillaRestApi.Controllers
     public class VillaApiController: ControllerBase
 
     {
+        // Get all villas in the database
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)] 
         public ActionResult<IEnumerable<VillaDto>> GetVillas() {
             return Ok(VillaDatastore.VillaList);
         }
 
+
+        // Get a single villa from the database 
+
         [HttpGet("{id:int}")]
         //[ProducesResponseType(200, Type = typeof(VillaDto))]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
-        public ActionResult<VillaDto> getVilla(int id)
+        public ActionResult<VillaDto> GetVilla(int id)
         { 
             if(id == 0)
             {
@@ -35,6 +39,31 @@ namespace VillaRestApi.Controllers
 
             return Ok(villa);
            
+        }
+
+        // Add new villa in the database
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto) {
+
+          if(villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }    
+
+          if(villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+          villaDto.Id = VillaDatastore.VillaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+
+          VillaDatastore.VillaList.Add(villaDto);
+
+          return Ok(villaDto);
         }
     }
 }
